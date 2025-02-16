4d6c63598adb59fcbbf48cdbc7ab94fc52900192
Ajay <ajay@extensioncrm.com>
2025-02-16 23:12:20 +0530

    Fix issue

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -10,6 +10,36 @@ class PropertyController {
   // Create a new Property
   async addProperty(req, res) {
     try {
+      const { title, category, price, location } = req.body;
+      if (!title) {
+        return sendResponse(res, getRequiredResponse("title"));
+      }
+      if (!category) {
+        return sendResponse(res, getRequiredResponse("category"));
+      }
+      if (!price) {
+        return sendResponse(res, getRequiredResponse("price"));
+      }
+      // Location validation
+      if (!location) {
+        return sendResponse(res, getRequiredResponse("location"));
+      }  if (!location.address) {
+        return sendResponse(res, getRequiredResponse("address"));
+      }  if (!location.city) {
+        return sendResponse(res, getRequiredResponse("city"));
+      }  if (!location.state) {
+        return sendResponse(res, getRequiredResponse("state"));
+      }  if (!location.pincode) {
+        return sendResponse(res, getRequiredResponse("pincode"));
+      }
+      // Coordinates validation
+      if (
+        !location.coordinates ||
+        location.coordinates.lat == null ||
+        location.coordinates.lng == null
+      ) {
+        return sendResponse(res, getRequiredResponse("lat or lng"));
+      }
       const response = await propertyServices.addProperty(req.body);
 
       return sendResponse(res, response);
--- services/property.service.js
+++ services/property.service.js
@@ -1,56 +1,16 @@
-const userModel = require("../models/property_model");
-const {
-  sendResponse,
-  getRequiredResponse,
-  getInternalErrorResponse,
-  getResponse,
-} = require("../utils/utils");
 const propertyModel = require("../models/property_model");
+const { getResponse, getInternalErrorResponse } = require("../utils/utils");
+
 class PropertyServices {
   async addProperty(propertyData) {
     try {
-      if (!propertyData) {
-        return sendResponse(res, getRequiredResponse("Data is required"));
-      }
-
-      const { title, category, price, location } = propertyData;
-      if (!title) {
-        return sendResponse(res, getRequiredResponse("title"));
-      }
-      if (!category) {
-        return sendResponse(res, getRequiredResponse("category"));
-      }
-      if (!price) {
-        return sendResponse(res, getRequiredResponse("price"));
-      }
-      // Location validation
-      if (!location) {
-        return sendResponse(res, getRequiredResponse("location"));
-      } else if (!location.address) {
-        return sendResponse(res, getRequiredResponse("address"));
-      } else if (!location.city) {
-        return sendResponse(res, getRequiredResponse("city"));
-      } else if (!location.state) {
-        return sendResponse(res, getRequiredResponse("state"));
-      } else if (!location.pincode) {
-        return sendResponse(res, getRequiredResponse("pincode"));
-      }
-      // Coordinates validation
-      if (
-        !location.coordinates ||
-        location.coordinates.lat == null ||
-        location.coordinates.lng == null
-      ) {
-        return sendResponse(res, getRequiredResponse("lat or lng"));
-      }
-      const newProperty = await propertyModel({
-        propertyData,
-      });
+      const newProperty = new propertyModel(propertyData);
 
       await newProperty.save();
+
       return getResponse(201, true, "Property added successfully", newProperty);
     } catch (e) {
-      log(e.message);
+      // Return error response
       return getInternalErrorResponse(e.message || "An error occurred");
     }
   }
